1977c5ec13c32a0ae2ba21ede472224834e9f494
Yohannees <yohannesgetachewerieso@gmail.com>
2023-08-16 01:19:40 +0300

    timer updated

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -7,7 +7,7 @@ interface TimerProps {
 const Timer: React.FC<TimerProps> = ({ initialTime }) => {
   const [remainingTime, setRemainingTime] = useState(initialTime);
   const [isRunning, setIsRunning] = useState(false);
-  const [inputTime, setInputTime] = useState(initialTime / 60000); 
+  const [inputTime, setInputTime] = useState(initialTime / 60000);
 
   useEffect(() => {
     let interval: number | null = null;
@@ -34,7 +34,7 @@ const Timer: React.FC<TimerProps> = ({ initialTime }) => {
   };
 
   const resetTimer = () => {
-    setRemainingTime(inputTime * 60000); 
+    setRemainingTime(inputTime * 60000);
     setIsRunning(false);
   };
 
@@ -49,20 +49,54 @@ const Timer: React.FC<TimerProps> = ({ initialTime }) => {
     return `${minutes}:${seconds.toString().padStart(2, "0")}`;
   };
 
+  const timerRadius = 70; // Set your desired radius for the circular timer
+
+  const animationStyles = {
+    animationDuration: `${initialTime / 60000}m`,
+    animationPlayState: isRunning ? "running" : "paused",
+  };
+
   return (
-    <div>
-      <h2>Timer</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          value={inputTime}
-          onChange={(e) => setInputTime(Number(e.target.value))}
-        />
-        <button type="submit">Set Timer</button>
-      </form>
-      <p>{formatTime(remainingTime)}</p>
-      <button onClick={toggleTimer}>{isRunning ? "Pause" : "Start"}</button>
-      <button onClick={resetTimer}>Reset</button>
+    <div className="text-center">
+      <h2 className="text-xl font-semibold mb-4">Timer</h2>
+      <div className="relative w-24 h-24">
+        <div
+          className="absolute border-4 border-blue-500 border-solid rounded-full w-full h-full"
+          style={animationStyles}
+        ></div>
+        <div className="absolute top-0 left-0 w-full h-full">
+          <form onSubmit={handleSubmit} className="flex flex-col items-center">
+            <input
+              type="number"
+              value={inputTime}
+              onChange={(e) => setInputTime(Number(e.target.value))}
+              className="w-16 mb-2 py-1 px-2 text-center border border-gray-300 rounded"
+            />
+            <span className="text-gray-500">minutes</span>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-1 px-4 mt-2 rounded hover:bg-blue-600 transition duration-300"
+            >
+              Set Timer
+            </button>
+          </form>
+          <p className="text-2xl font-semibold mt-4">
+            {formatTime(remainingTime)}
+          </p>
+          <button
+            onClick={toggleTimer}
+            className="bg-blue-500 text-white py-1 px-4 mt-2 rounded hover:bg-blue-600 transition duration-300"
+          >
+            {isRunning ? "Pause" : "Start"}
+          </button>
+          <button
+            onClick={resetTimer}
+            className="bg-red-500 text-white py-1 px-4 mt-2 rounded hover:bg-red-600 transition duration-300"
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
