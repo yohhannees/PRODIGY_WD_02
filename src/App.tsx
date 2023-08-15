c70d46ea9877090c390afecfe76592c41160f337
Yohannees <yohannesgetachewerieso@gmail.com>
2023-08-16 01:02:05 +0300

    tailwind css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 
-
-import './App.css'
+import "./index.css";
 
 function App() {
-
   return (
-    <>
-   <h1>hello</h1>
-    </>
-  )
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
 }
 
-export default App
+export default App;
