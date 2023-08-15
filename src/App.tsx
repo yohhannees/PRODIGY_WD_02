6f84c3f94f823c7669857a0c426448a096f87c34
Yohannees <yohannesgetachewerieso@gmail.com>
2023-08-16 01:22:41 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 
-import "./index.css";
 import Stopwatch from "../src/components/Stopwatch";
 import Timer from "../src/components/Timer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8">
       <Stopwatch />
-      <Timer initialTime={0}/>
+      <Timer initialTime={0} />
     </div>
   );
 }
--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -39,23 +39,58 @@ const Stopwatch: React.FC = () => {
     return `${minutes}:${seconds}`;
   };
 
+  const stopwatchRadius = 70; // Set your desired radius for the circular stopwatch
+
+  const animationStyles = {
+    animationPlayState: isRunning ? "running" : "paused",
+  };
+
   return (
-    <div>
-      <h2>Stopwatch</h2>
-      <p>{formatTime(elapsedTime)}</p>
-      {isRunning && <button onClick={startStopwatch}>Lap</button>}
-      <button onClick={startStopwatch}>{isRunning ? "Stop" : "Start"}</button>
-      <button onClick={resetStopwatch}>Reset</button>
-      {laps.length > 0 && (
-        <div>
-          <h3>Laps</h3>
-          <ul>
-            {laps.map((lapTime, index) => (
-              <li key={index}>{formatTime(lapTime)}</li>
-            ))}
-          </ul>
+    <div className="text-center">
+      <h2 className="text-xl font-semibold mb-4">Stopwatch</h2>
+      <div className="relative w-24 h-24">
+        <div
+          className="absolute border-4 border-blue-500 border-solid rounded-full w-full h-full"
+          style={animationStyles}
+        ></div>
+        <div className="absolute top-0 left-0 w-full h-full">
+          <p className="text-2xl font-semibold mt-4">
+            {formatTime(elapsedTime)}
+          </p>
+          {isRunning && (
+            <button
+              onClick={startStopwatch}
+              className="bg-blue-500 text-white py-1 px-4 mt-2 rounded hover:bg-blue-600 transition duration-300"
+            >
+              Lap
+            </button>
+          )}
+          <button
+            onClick={startStopwatch}
+            className="bg-blue-500 text-white py-1 px-4 mt-2 rounded hover:bg-blue-600 transition duration-300"
+          >
+            {isRunning ? "Stop" : "Start"}
+          </button>
+          <button
+            onClick={resetStopwatch}
+            className="bg-red-500 text-white py-1 px-4 mt-2 rounded hover:bg-red-600 transition duration-300"
+          >
+            Reset
+          </button>
+          {laps.length > 0 && (
+            <div className="mt-4">
+              <h3 className="text-lg font-semibold mb-2">Laps</h3>
+              <ul>
+                {laps.map((lapTime, index) => (
+                  <li key={index} className="text-md font-medium text-gray-600">
+                    {formatTime(lapTime)}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
